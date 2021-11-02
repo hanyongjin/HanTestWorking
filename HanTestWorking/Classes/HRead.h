af618e6aff64684289b0b2f05fdca636d22e67d4
han <1018971016@qq.com>
2021-11-02 17:50:57 +0800

    afn

---
+++ HanTestWorking/Classes/HRead.h
@@ -0,0 +1,17 @@
+//
+//  HRead.h
+//  HanTestWorking_Example
+//
+//  Created by han on 2021/11/2.
+//  Copyright © 2021 1018971016@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HRead : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HanTestWorking/Classes/HRead.m
@@ -0,0 +1,13 @@
+//
+//  HRead.m
+//  HanTestWorking_Example
+//
+//  Created by han on 2021/11/2.
+//  Copyright © 2021 1018971016@qq.com. All rights reserved.
+//
+
+#import "HRead.h"
+
+@implementation HRead
+
+@end
